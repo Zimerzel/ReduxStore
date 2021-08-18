476de87e59b016e223f43f608cd983d938c345ac
Imerzel <zach.imerzel@gmail.com>
2021-08-18 10:55:20 -0500

    added hooks to catmenu

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -4,10 +4,14 @@ import { useQuery } from '@apollo/react-hooks';
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
+import { useSelector, useDispatch } from "react-redux";
 
 
 function CategoryMenu() {
   // const [state = initalState, dispatch] = useStoreContext();
+  const state = useSelector(state => state)
+
+  const dispatch = useDispatch();
 
   const { categories } = state;
 
